7eeb6706b9d5fe54a3e363704f1063ad94a3935e
Naht03 <00001118569945SP@al.educacao.sp.gov.br>
2024-10-29 13:55:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como calcular a área de um triangulo?',
+    'Multiplicando a base pela altura e dividindo o resultado por dois'
+)
+
+criaCartao(
+    'Português',
+    'O que é um verbo?',
+    'O verbo é a palavra que expressa ações, estados e fenômenos da natureza.'
+)
